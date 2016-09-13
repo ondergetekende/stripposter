c3a6229e6912b574b8f4060331e096151d6d12eb
Koert van der Veer <koert@ondergetekende.nl>
2016-09-13 21:21:39 +0200

    Implement Fokke en Sukke
    Closes #2

--- stripbot/comics.py
+++ stripbot/comics.py
@@ -1,4 +1,5 @@
 import datetime
+from urllib.parse import urlparse, parse_qs
 
 from .base import BaseComicModel
 
@@ -42,3 +43,26 @@ class Jeroom(BaseComicModel):
         date = self.element.cssselect('.desc')[0].text
         self.id = date
         self.date = datetime.datetime.strptime(date, "%d-%m-%Y")
+
+
+class FokkeEnSukke(BaseComicModel):
+    base_url = "http://www.foksuk.nl/nl"
+    name = "Fokke en Sukke"
+    author = " Jean-Marc van Tol"
+
+    css_selector = '.cartoon'
+
+    def extract_comic(self):
+        img = self.element.cssselect('img[src^=content]')[0]
+        self.page_url = self.element.cssselect('a.active')[0].get('href')
+        print(self.page_url)
+        self.image_url = img.get('src')
+        self.title = img.get('alt').rsplit('(', 1)[0]
+
+        qs = urlparse(self.page_url).query
+        qs = dict(parse_qs(qs))
+        print (qs)
+        date_sec = int(qs['ctime'][0])
+        self.id = date_sec
+        self.date = (datetime.datetime(1970, 1, 1) + 
+                     datetime.timedelta(seconds=date_sec))
